b197d290d6afa2bd62257c09e257982a03280024
Faldass <quentin.terzi@outlook.fr>
2026-08-21 12:04:17 +0200

    Correction pb DB

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 const Database = require("better-sqlite3");
 const path = require("path");
+const { gameDayStr } = require("./slotUtils");
 const fs = require("fs");
 
 const dataDir = path.join(__dirname, "..", "data");
